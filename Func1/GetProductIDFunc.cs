01f97cecccc13021a50041827e78c807d584f49d
Daniel Kondrashevich <dakondra@microsoft.com>
2020-09-04 16:19:11 -0700

    Test ci cd

--- Func1/GetProductIDFunc.cs
+++ Func1/GetProductIDFunc.cs
@@ -27,7 +27,7 @@ namespace OpenHackCo.Function
 
             string responseMessage = string.IsNullOrEmpty(productId)
                 ? "This HTTP triggered function executed successfully. Pass a product id in the query string or in the request body for a personalized response."
-                : $"The product name for your product id {productId} is Starfruit Explosion";
+                : $"The product name for your product id {productId} is Starfruit Explosion. A NEW COMMIT";
 
             return new OkObjectResult(responseMessage);
         }
